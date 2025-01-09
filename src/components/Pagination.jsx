5f3f155289b6a6bef41ad0ba9b17862c08d3c1aa
Yusufrhman <zvistyt@gmail.com>
2025-01-10 06:10:49 +0700

    pagination bug fixed

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,6 @@
 import { IoIosArrowBack } from "react-icons/io";
 
 export default function Pagination({ currentPage, totalPages, onPageChange }) {
-
   let pages = [];
   if (totalPages > 6) {
     if (currentPage < 4) {
@@ -29,11 +28,12 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
   } else {
     pages = Array.from({ length: totalPages }, (_, i) => i + 1);
   }
+  console.log(pages);
 
   return (
     <div className="flex justify-center mt-5 space-x-2">
       <button
-        key="prev"
+        key="pagination-prev"
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
         className={`px-3 py-2 md:px-4 md:py-2 text-xs sm:text-sm md:text-base rounded-lg shadow-sm font-medium transition-all duration-300 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-teal-100 dark:hover:bg-teal-600 disabled:opacity-50 disabled:hover:dark:bg-gray-700 disabled:hover:bg-gray-200`}
@@ -42,7 +42,7 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
       </button>
       {pages.map((page, index) => (
         <button
-          key={page}
+          key={`${page}-${index}`}
           onClick={() => page !== "..." && onPageChange(page)}
           disabled={page === "..." || page === currentPage}
           className={`px-3 py-2 md:px-4 md:py-2 text-xs sm:text-sm md:text-base rounded-lg shadow-sm font-medium transition-all duration-300 ${
@@ -55,7 +55,7 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
         </button>
       ))}
       <button
-        key="next"
+        key="pagination-next"
         onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
         className={`px-3 py-2 md:px-4 md:py-2 text-xs sm:text-sm md:text-base rounded-lg shadow-sm font-medium transition-all duration-300 bg-gray-200 dark:bg-gray-700 text-gray-700 dark:text-gray-300 hover:bg-teal-100 dark:hover:bg-teal-600 disabled:opacity-50 disabled:hover:dark:bg-gray-700 disabled:hover:bg-gray-200`}
